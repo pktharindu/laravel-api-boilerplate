a203cf69155d728b9d4fa1a153c1b767760e53bd
P. K. Tharindu <pktharindu@outlook.com>
2021-06-03 12:45:26 +0530

    update setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,6 @@
 
 cp .env.example .env
 composer install
+read -r -n 1 -p "Please fill out the environment variables in the .env file and press any key to continue... "
+echo
 php artisan migrate --seed
