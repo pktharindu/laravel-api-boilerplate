5a5584b5a20f944fdf274f079169a7be9c2a35bb
P. K. Tharindu <pktharindu@outlook.com>
2021-06-09 13:49:27 +0530

    add blueprint:init to setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,3 +6,4 @@ php artisan key:generate
 read -r -n 1 -p "Please fill out the environment variables in the .env file and press any key to continue... "
 echo
 php artisan migrate --seed
+php artisan blueprint:init
